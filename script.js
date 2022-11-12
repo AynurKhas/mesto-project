df50b36c483d2bbc22734c32064c96e6ff80cacf
Aynur Khas <InurHasanov@yandex.ru>
2022-11-12 17:40:22 +0300

    осталось js файл отдельно подключить

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const popup = document.querySelectorAll('.popup');
 const popupClose = document.querySelectorAll('.popup__close');
 
-const popupProfileAdd = document.querySelector('.popup__profileAdd');
-const popupCardAdd = document.querySelector('.popup__cardAdd');
+const popupProfileAdd = document.querySelector('.popup_profileAdd');
+const popupCardAdd = document.querySelector('.popup_cardAdd');
 
 const profileEdit = document.querySelector('.profile__edit-link');
 const formName = document.querySelector('.form__name');
 const formProfession = document.querySelector('.form__profession');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
-const popupProfile = document.querySelector('.popup_profile');
+// const popupProfile = document.querySelector('.popup_profile');
 
 const profileAdLink = document.querySelector('.profile__add-link');
-const popupAddPlace = document.querySelector('.popup_add-place');
+// const popupAddPlace = document.querySelector('.popup_add-place');
 
 const cardTemplate = document.querySelector('#card-template').content;
 const elementsList = document.querySelector('.elements__list');
@@ -23,10 +23,10 @@ const elementsGroupTitle = cardTemplate.querySelector('.elements__group-title');
 const formPlace = document.querySelector('.form__place');
 const formLinkPlace = document.querySelector('.form__link-place');
 
-const elementsListItem = document.querySelector('.elements__list-item');
+const formAddProfile = document.querySelector('.form_add-profile');
 
 
-
+// import {initialCards} from "./card"
 const initialCards = [
   {
     name: 'Архыз',
@@ -58,9 +58,9 @@ const initialCards = [
 
 // open
 
-function open(popup) {
-  popup.classList.add('popup_opened');
-};
+function open(item) {
+  item.classList.add('popup_opened');
+}
 
 profileEdit.addEventListener('click', () => {
   open(popupProfileAdd);
@@ -78,15 +78,14 @@ profileAdLink.addEventListener('click', () => {
 function close() {
   popup.forEach((item) => {
     item.classList.remove('popup_opened');
-  })
+  });
 }
 
 popupClose.forEach((item) => {
-  item.addEventListener('click', close)
-})
+  item.addEventListener('click', close);
+});
 
 
-const formAddProfile = document.querySelector('.form__add-profile');
 formAddProfile.addEventListener('submit', (evt) => {
   evt.preventDefault();
   profileName.textContent = formName.value;
@@ -100,27 +99,35 @@ function addCard(formPlaceValue, formLinkPlaceValue) {
   elementsItemImage.setAttribute('src', formLinkPlaceValue);
   const elementsListItem = cardTemplate.querySelector('.elements__list-item').cloneNode(true);
   const elementsButton = elementsListItem.querySelector('.elements__button');
+
   elementsButton.addEventListener('click', function (evt) {
     evt.target.classList.toggle('elements__button_active');
   });
   const elementsTrash = elementsListItem.querySelector('.elements__trash');
   elementsTrash.addEventListener('click', function () {
     elementsListItem.remove();
-    });
-    elementsList.prepend(elementsListItem);
-  }
+  });
+  elementsList.prepend(elementsListItem);
+  const popupCard = document.querySelector('.popup_card');
+  elementsListItem.querySelector('.elements__item-image').addEventListener('click', () => {
+    open(popupCard);
+    document.querySelector('.popup__card-image').src = elementsListItem.querySelector('.elements__item-image').getAttribute('src');
+    document.querySelector('.popup__card-caption').textContent = elementsListItem.querySelector('.elements__group-title').textContent;
+    document.querySelector('.popup_card').style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+  });
+}
 
 initialCards.forEach(function (item) {
-    addCard(item.name, item.link);
-  })
+  addCard(item.name, item.link);
+});
 
-const formAddPlace = document.querySelector('.form__add-place');
-  formAddPlace.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    addCard(formPlace.value, formLinkPlace.value);
-    formPlace.value = '';
-    formLinkPlace.value = '';
-    close();
-  });
+const formAddPlace = document.querySelector('.form_add-place');
+formAddPlace.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  addCard(formPlace.value, formLinkPlace.value);
+  formPlace.value = '';
+  formLinkPlace.value = '';
+  close();
+});
 
 
