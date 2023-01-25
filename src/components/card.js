863d105e2f8242f58a7246d005e09b909b901ce5
Aynur Khas <InurHasanov@yandex.ru>
2023-01-25 22:54:54 +0300

    сделать функцию удаления карточки через сабмит

--- src/components/card.js
+++ src/components/card.js
@@ -1,40 +1,16 @@
-import { openPopup } from "./modal.js";
+import { closePopup, openPopup } from "./modal.js";
 import { checkWhoOwns, checkMyLikes } from "./utils";
-import { addLike, deleteLike } from "./api";
-/*const cards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];*/
+import { addLike, deleteLike, deleteCard } from "./api";
 
 const popupCard = document.querySelector('.popup_card');
 const captionPopupCard = document.querySelector('.popup__card-caption');
 const elementsContainer = document.querySelector('.elements__list');
 const cardTemplate = document.querySelector('#card-template').content;
 const imagePopupCard = document.querySelector('.popup__card-image');
+const popupDelete = document.querySelector('.popup_delete');
+const formDelete = document.querySelector('.form_delete');
 
-// ------------------------------------------- Готовая катрочка
+// ------------------------------------------- Создание карточки
 export function addCard(object) {
   const card = cardTemplate.querySelector('.elements__list-item').cloneNode(true);
   const cardImage = card.querySelector('.elements__item-image');
@@ -44,12 +20,13 @@ export function addCard(object) {
   cardImage.setAttribute('src', object.link);
   cardImage.setAttribute('alt', object.name);
   cardLikeCounter.textContent = object.likes.length;
+
   // ------------------------------------------- кнопка Нравиться
   const btnlike = card.querySelector('.elements__button');
-//проверка наличия отмеченного мною лайка
-    if (checkMyLikes(object.likes)) {
-      btnlike.classList.add('elements__button_active');
-    }
+  //проверка наличия отмеченного мною лайка при открытии страницы
+  if (checkMyLikes(object.likes)) {
+    btnlike.classList.add('elements__button_active');
+  }
 
   btnlike.addEventListener('click', function (evt) {
     evt.target.classList.toggle('elements__button_active');
@@ -77,9 +54,24 @@ export function addCard(object) {
     elementsTrash.classList.add('elements__trash_disabled');
     elementsTrash.setAttribute("disabled", true);
   }
+
   elementsTrash.addEventListener('click', function () {
-    card.remove();
-  });
+    const deleteCardId = object._id;
+    openPopup(popupDelete);
+
+    //-------------------------------------------удаление карточки по кнопке ДА
+    formDelete.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      deleteCard(deleteCardId)
+        .then((result) => {
+        })
+        .catch((err) => {
+          console.log((err));
+        });
+        card.remove();
+        closePopup();
+    });
+  })
   // ------------------------------------------- попап по нажатию на изображение
   card.querySelector('.elements__item-image').addEventListener('click', () => {
     openPopup(popupCard);
@@ -95,12 +87,11 @@ export function addCard(object) {
 export function renderCard(index) {
   elementsContainer.prepend(index);
 }
-// ------------------------------------------- Добавления карточек из файла card.js
-/*export function initialCards() {
-  cards.forEach(function (item) {
-    renderCard(addCard(item.name, item.link));
-  });
-}*/
+// // ------------------------------------------- Функция добавления карточки в конец html
+export function renderCardToEnd(index) {
+  elementsContainer.append(index);
+}
+
 
 // ------------------------------------------- Добавления карточек из файла сервера
 export function initialCards(object) {
