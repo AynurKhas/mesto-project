81f38031691e32a72734b7282e02e1ecd33a152f
Aynur Khas <InurHasanov@yandex.ru>
2023-02-14 21:22:21 +0300

    добавил классы Валидации

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,3 +1,50 @@
+export class Popup {
+  constructor(selector) {
+    this._selector = selector;
+  }
+
+  _handleEscClose() {
+      if (evt.key === 'Escape') {
+        this.close();
+      }
+  }
+
+  setEventListeners() {
+    if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close')) {
+      this.close();
+    }
+  }
+
+  open() {
+    this._selector.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose(evt)); // !!!
+    this._selector.addEventListener('mousedown', setEventListeners(evt));   // !!!
+  }
+
+  close() {
+    const popupOpen = document.querySelector('.popup_opened');
+    if (popupOpen) {
+      popupOpen.classList.remove('popup_opened');
+      document.removeEventListener('keydown', this._handleEscClose(evt));    // !!!
+      popupOpen.removeEventListener('mousedown', setEventListeners(evt)); // !!!
+    }
+  }
+}
+
+export class PopupWithImage extends Popup{
+  constructor(selector) {
+    super(selector);
+  }
+
+  open() {
+    imagePopupCard.src = object.link; // импорт
+    imagePopupCard.alt = object.name; // импорт
+    captionPopupCard.textContent = object.name; // импорт
+    super.open();
+  }
+}
+
+
 
 // ------------------------------------------- Функция открытия
 export function openPopup(item) {
